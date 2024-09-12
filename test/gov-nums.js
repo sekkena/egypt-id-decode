9903b7083bdd7a7979946c0f146a36e08879ead8
Sekkena2 <153958398+sekkena@users.noreply.github.com>
2024-09-12 03:52:52 +0300

    Add files via upload

---
+++ test/gov-nums.js
@@ -0,0 +1,34 @@
+/*
+___________________________
+| Code | Governorate      |
+|------|------------------|
+| 01   | Cairo            |
+| 02   | Alexandria       |
+| 03   | Port Said        |
+| 04   | Suez             |
+| 11   | Damietta         |
+| 12   | Dakahlia         |
+| 13   | Sharqia          |
+| 14   | Qalyubia         |
+| 15   | Kafr El Sheikh   |
+| 16   | Gharbia          |
+| 17   | Monufia          |
+| 18   | Beheira          |
+| 19   | Ismailia         |
+| 21   | Giza             |
+| 22   | Beni Suef        |
+| 23   | Fayoum           |
+| 24   | Minya            |
+| 25   | Assiut           |
+| 26   | Sohag            |
+| 27   | Qena             |
+| 28   | Aswan            |
+| 29   | Luxor            |
+| 31   | Red Sea          |
+| 32   | New Valley       |
+| 33   | Matruh           |
+| 34   | North Sinai      |
+| 35   | South Sinai      |
+===========================
+*/
+//package egypt-id-decode made by sekkena
---
+++ test/sex-num.js
@@ -0,0 +1,17 @@
+/*
+___________________________
+| Code | Gender           |
+|------|------------------|
+| 1    | male             |
+| 3    | male             |
+| 5    | male             |
+| 7    | male             |
+| 9    | male             |
+| 0    | female           |
+| 2    | female           |
+| 4    | female           |
+| 6    | female           |
+| 8    | female           |
+===========================
+*/
+//package egypt-id-decode made by sekkena
